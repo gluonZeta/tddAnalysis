953f859381d332d0fb7e2681cb560b7619a7b6f7
Gluon <andrainat@gmail.com>
2024-03-02 21:15:08 +0100

    Test of the context load

--- src/test/java/gluon/projects/TddAnalysisApplicationTests.java
+++ src/test/java/gluon/projects/TddAnalysisApplicationTests.java
@@ -2,14 +2,25 @@ package gluon.projects;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.ApplicationContextFactory;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.assertj.ApplicationContextAssertProvider;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.AUTO_CONFIGURED)
 class TddAnalysisApplicationTests {
 
 	@Test
-	void contextLoads() {
-		Assertions.assertTrue(true);
+	void contextLoads(ApplicationContext context) {
+		Assertions.assertNotNull(context);
+		/*
+		String[] beanNames = context.getBeanDefinitionNames();
+		Arrays.stream(beanNames).filter(x -> !x.contains("org.springframework")).forEach(System.out::println);
+		 */
 	}
 
 }
